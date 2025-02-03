a665670b0bc4954291e3a7868845bc8f18e8c2b0
CanonLs <467706369@qq.com>
2025-02-03 20:26:47 +0800

    #

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import type { UserConfig } from 'vite'
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()] as UserConfig['plugins'],
-  build: {
-    ssr: true,
-    rollupOptions: {
-      input: {
-        app: './index.html',
-        server: './src/entry/server.tsx'
-      }
-    }
-  }
+  plugins: [react()],
 })
