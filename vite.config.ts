49158d8850e98064e80ed9dc4de9d986cbd7a04f
CanonLs <467706369@qq.com>
2025-02-03 16:04:43 +0800

    开启ssr

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import vercel from 'vite-plugin-vercel';
+import vercel from 'vite-plugin-vercel'
 
-
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), vercel()],
+  plugins: [
+    react(),
+    vercel({
+      // 开启 SSR
+      ssr: true,
+      // 指定 SSR 入口文件
+      serverEntry: 'src/entry-server.tsx'
+    })
+  ]
 })
