65cabe3fc0fb8e704a5341af1a5ce4f0382cae99
CanonLs <467706369@qq.com>
2025-02-04 00:30:00 +0800

    #

---
+++ .vercel/output/static/entry-server.js
@@ -0,0 +1,34 @@
+import { jsxs, Fragment, jsx } from "react/jsx-runtime";
+import { useState, StrictMode } from "react";
+import ReactDOMServer from "react-dom/server";
+function App() {
+  const [count, setCount] = useState(0);
+  return /* @__PURE__ */ jsxs(Fragment, { children: [
+    /* @__PURE__ */ jsxs("div", { children: [
+      /* @__PURE__ */ jsx("a", { href: "https://vite.dev", target: "_blank", children: /* @__PURE__ */ jsx("img", { src: "/vite.svg", className: "logo", alt: "Vite logo" }) }),
+      /* @__PURE__ */ jsx("a", { href: "https://reactjs.org", target: "_blank" })
+    ] }),
+    /* @__PURE__ */ jsx("h1", { children: "Vite + React" }),
+    /* @__PURE__ */ jsxs("div", { className: "card", children: [
+      /* @__PURE__ */ jsxs("button", { onClick: () => setCount((count2) => count2 + 1), children: [
+        "count is ",
+        count
+      ] }),
+      /* @__PURE__ */ jsxs("p", { children: [
+        "Edit ",
+        /* @__PURE__ */ jsx("code", { children: "src/App.tsx" }),
+        " and save to test HMR"
+      ] })
+    ] }),
+    /* @__PURE__ */ jsx("p", { className: "read-the-docs", children: "Click on the Vite and React logos to learn more" })
+  ] });
+}
+function render(_url) {
+  const html = ReactDOMServer.renderToString(
+    /* @__PURE__ */ jsx(StrictMode, { children: /* @__PURE__ */ jsx(App, {}) })
+  );
+  return { html };
+}
+export {
+  render
+};
